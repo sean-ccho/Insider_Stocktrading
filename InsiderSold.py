7140d8be9c7a9d3d476bc02aad2e0e346c412612
Sean Cho <seancho@Seans-Mac-Mini-Personal.local>
2023-08-09 11:33:55 -0400

    WebDriver - Manual update

--- InsiderSold.py
+++ InsiderSold.py
@@ -18,7 +18,9 @@ class insidetracksold:
         options = Options()
         options.headless = True
         current_time = now.strftime("%H:%M:%S")
-        driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+        chrome_driver_path = '/Users/seancho/Documents/Code/Insider_Stocktrading/chromedriver'
+        driver = webdriver.Chrome(executable_path=chrome_driver_path)
+        # driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
         url = 'https://ceo.ca/api/sedi/?symbol=&amount=&transaction=&insider='
         googlesheeturl = 'https://docs.google.com/spreadsheets/d/12DBEu_hWAquzwGcX_K3sUWU45TLrEQw278S_r_tRxng/edit#gid=0'
         driver.get(url)
